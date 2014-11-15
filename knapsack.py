b748e22660dcb4ca91328a6af5bdf865ba661eef
skmullins <skmllns@gmail.com>
2014-11-14 21:47:39 -0500

    added Box class

--- knapsack.py
+++ knapsack.py
@@ -5,9 +5,15 @@
 ## as to minimize the total weight and maximize the total value within?
 ############################################################################
 
-import defs
 import random
 
+# not sure that this needs its own def file right now
+class Box():
+    def __init__(self, id, weight, value):
+        self.id = id
+        self.weight = weight
+        self.value = value
+        
 # knapsack parameters
 capacity = 50
 num_boxes = 10
@@ -23,7 +29,7 @@ contents = list()
 for i in range(num_boxes):
     weight = random.randint(1, 30)
     value = random.randint(1, 10)
-    box = defs.Box(i, weight, value)
+    box = Box(i, weight, value)
     collection.append(box)
 
 
